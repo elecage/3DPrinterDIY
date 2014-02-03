28e439f900b96676ba11c7b117892875faf15997
elecage <elecage@gmail.com>
2014-02-03 19:53:10 +0900

    Configuration 재수정

--- Marlin-Marlin_v1/Marlin/Configuration.h
+++ Marlin-Marlin_v1/Marlin/Configuration.h
@@ -387,7 +387,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // modified by elecage /// set to tr
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,2000,1120} // 775  // modified by elecage // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,2000,785} // 775  // modified by elecage // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {400, 400, 1, 45}   // modified by elecage // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {5000,5000,10,5000}  // modified by elecage  // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
